5b37b83c31e357ca60f47836fb24b0161153550b
SavaOurl <dendiroblek@gmail.com>
2023-10-31 18:46:12 +0400

    added lab5

---
+++ lab5.py
@@ -0,0 +1,52 @@
+import random
+#сортируем массив от малого к большему
+def bubbleSort(arr):
+    arrRange = len(arr)
+    for i in range(arrRange):
+        if(i == 0):
+            for j in range(arrRange - 1):
+                        if arr[j] > arr[j + 1]: 
+                            arr[j], arr[j + 1] = arr[j + 1], arr[j] #двигает элемент,и так как самый большой уже в конце,то смысла делать ещё итер нет
+        else:
+            for j in range(arrRange - i):
+                    if arr[j] > arr[j + 1]: 
+                        arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+#находим ключ
+def findKey(arr, key):
+    n = len(arr)
+    
+    for i in range(n):
+        if arr[i] == key:
+            print(f'index {i} number {arr[i]}')
+    
+    return -1
+    
+        
+def lab5(sizeOfList,startZone,finishZone):
+    list = []
+    #fill our list
+    for x in range(sizeOfList):
+        randomNumber = random.randint(startZone, finishZone)
+        list.append(randomNumber)
+    print(list)
+    #sorting
+    bubbleSort(list)
+    print(list)
+    #find key
+    print(findKey(list,5))
+    
+
+lab5(10,-111,110)
+#другой метод
+def vstavkiSort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]  # берем 2ой элемент
+        j = i -  1
+        #проверяем является ли элемент до нынешнего больше
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j] # если так, то заменяем его (здесь key это (arr[j + 1])) на элемент позади (arr[j])
+            j -= 1 #убавляем до -1 индекса,чтобы обращаться к тому же элементу,но только теперь он на позиции ниже
+            
+        arr[j + 1] = key #взависимости от исхода присваеваем элементу кей
+    return arr   
